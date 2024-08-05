b5013a0ef9c3b93cdbc7f99b22c80697b789f441
fashton28 <fashton502@gmail.com>
2024-08-05 19:17:09 -0400

    build basic react components

---
+++ src/Greet.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {Component} from 'react';
+
+class Greet extends Component {
+  render() {
+    return (
+      <div>
+            <h1>greetings from Greet File!</h1>
+      </div>
+    );
+  }
+}
+
+export default Greet;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
+import Greet from './Greet'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Hello greeting = {'HELLO FROM PROPERTY'} />
   </React.StrictMode>
 );
 
